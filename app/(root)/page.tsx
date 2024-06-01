39d5d095d6e03b4c2e03c4a2580f1e946bc99757
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-01 13:14:07 +0100

    podcast card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,24 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => (
+            <PodcastCard
+              key={podcast.id}
+              imgUrl={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
