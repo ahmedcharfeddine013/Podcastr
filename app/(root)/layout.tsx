5d9adcd636694908e3448a4a668769aa4431bfcc
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-01 12:41:12 +0100

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,13 @@
+import LeftSidebar from "../../components/LeftSidebar";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        LEFT SIDEBAR
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
         {children}
         RIGHT SIDEBAR
       </main>
---
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PodcastDetails = ({
+  params: { podcastId },
+}: {
+  params: { podcastId: string };
+}) => {
+  return <div></div>;
+};
+
+export default PodcastDetails;
