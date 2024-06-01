dfa347ecb52dff23659b4d91d8e50fe33936f197
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-01 13:42:40 +0100

    convex setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import Image from "next/image";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
